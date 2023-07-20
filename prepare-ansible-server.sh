fc6918091108cd80c01045a71bbec3f48454f7b3
nitheesh-1612 <116325479+nitheesh-1612@users.noreply.github.com>
2023-07-20 14:58:08 +0530

    Update prepare-ansible-server.sh

--- prepare-ansible-server.sh
+++ prepare-ansible-server.sh
@@ -1,1 +1,8 @@
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+sudo apt install python3-pip -y
+sudo pip3 install boto3 botocore
 
